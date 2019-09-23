e50fcf252839fb3a3b9cf80970dc0da19cdfe05b
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-09-23 11:32:38 +0000

    update files

--- config.py
+++ config.py
@@ -2,17 +2,17 @@ import os
 
 class Config:
 
-	user = "gokul"
-	lang = "hindi"
+    user = "gokul"
+    lang = "hindi"
 
 
-	image_size = [512, 512, 3]
-	geometry = "QUAD" # ["RBOX", "QUAD"]
-	label_method = "single" # ["single", "multiple"]
+    image_size = [512, 512, 3]
+    geometry = "QUAD" # ["RBOX", "QUAD"]
+    label_method = "single" # ["single", "multiple"]
 
-	max_m_train = 10000
-	data_dir = "/home/{}/data-split/{}".format(user, lang)
-	train_data_dir = os.path.join(data_dir, 'train')
+    max_m_train = 10000
+    data_dir = "/home/{}/data-split/{}".format(user, lang)
+    train_data_dir = os.path.join(data_dir, 'train')
     dev_data_dir = os.path.join(data_dir, 'dev')
     test_data_dir = os.path.join(data_dir, 'test')
 
--- dataset.py
+++ dataset.py
@@ -6,6 +6,11 @@ import torchvision
 import os
 import cv2
 import csv
+import numpy as np
+
+from tqdm import tqdm
+import time
+
 
 """
 Reference: https://github.com/liushuchun/EAST.pytorch
@@ -19,7 +24,7 @@ image_size = config['image_size']
 n_H, n_W, n_C = image_size
 
 if geometry == "RBOX":
-	raise NotImplementedError("Only implemented for the QUAD geometry")
+    raise NotImplementedError("Only implemented for the QUAD geometry")
 if label_method == "multiple":
     raise NotImplementedError("Only implemented for the single label method")
 
@@ -35,14 +40,14 @@ def list_images(images_dir):
 
 def quads_to_rboxes(quads_coords):
 
-	pass
+    raise NotImplementedError()
 
 
-def load_coords(annotation_path):
+def load_shapes_coords(annotation_path):
 
-	"""
-	> correct the order of the coords of a quad
-	"""
+    """
+    > correct the order of the coords of a quad
+    """
 
     quads_coords = []
     
@@ -52,57 +57,97 @@ def load_coords(annotation_path):
             text = line[-1]
             x1, y1, x2, y2, x3, y3, x4, y4 = list(map(float, line[:8]))
             quad_cords = [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]
-            quads_coords_c.append(quad_cords)
+            quads_coords.append(quad_cords)
     quads_coords = np.array(quads_coords, dtype=np.float32)
     
     if geometry == "QUAD":
-    	coords = quads_coords
+        shapes_coords = quads_coords
     elif geometry == "RBOX":
-    	coords =  quads_to_rboxes(coords) 
+        shapes_coords =  quads_to_rboxes(coords) 
     
-    return coords
+    return shapes_coords
 
 
 def load_image(image_path):
 
-	image = cv2.imread(image_path)
-	image = image[:, :, ::-1] # BGR to RGB
+    image = cv2.imread(image_path)
+    image = image[:, :, ::-1] # BGR to RGB
     image = image.astype(np.float32)
-
-	return image
+    image = np.moveaxis(image, 2, 0) # channel_last to channel_first
+    return image
 
 
 def load_score_and_geometry_map(annotation_path):
 
-	coords = load_coords(annotation_path)
-
-    if label_method == "simple":
-
+    shapes_coords = load_shapes_coords(annotation_path)
+    score_map = np.zeros([128, 128, 1])
+    geometry_map = np.zeros([128, 128, 8])
+    score_map_raw = np.zeros([n_H, n_W, 1])
+    geometry_map_raw = np.zeros([n_H, n_W, 8])
+    
+    if label_method == "single":
+        if geometry == "QUAD":
+            
+            shapes_centre = np.mean(shapes_coords, axis=1).astype(np.int32)
+            #print("shapes_coords", shapes_coords.shape, "\n", shapes_coords)
+            #print("shapes_centre", shapes_centre.shape, "\n", shapes_centre)
+            for shape_coords, shape_centre in zip(shapes_coords, shapes_centre): # shape_coords -> [4, 2], shape_centre -> [2]
+                c_h, c_w = shape_centre
+                score_map_raw[c_h, c_w, 0] = 1
+                geometry_map_raw[c_h, c_w] = shape_coords.flatten() # [8]
+           
+            score_map_raw = np.moveaxis(score_map_raw, 2, 0) # channel_last to channel_first
+            geometry_map_raw = np.moveaxis(geometry_map_raw, 2, 0) # channel_last to channel_first
+            
+            max_pool_2d = nn.MaxPool2d((4,4), stride=4)
+            score_map_raw = torch.from_numpy(score_map_raw)
+            score_map = max_pool_2d(score_map_raw)
+            geometry_map_raw = torch.from_numpy(geometry_map_raw)
+            geometry_map = max_pool_2d(geometry_map_raw)
+                                       
+            #print("score_map", score_map.shape, "\n", score_map.sum())
+            #print("geometry_map", geometry_map.shape, "\n", (geometry_map.sum(axis=0)>0).sum())
+            #time.sleep(10)
+                                       
+        elif geometry == "RBOX":
+            raise NotImplementedError()
+        else:
+            raise ValueError("Invalid geometry: " + geometry)
 
     elif label_method == "multiple":
-        pass
+        if geometry == "QUAD":
+            raise NotImplementedError()
+        elif geometry == "RBOX":
+            raise NotImplementedError()            
+        else:
+            raise ValueError("Invalid geometry: " + geometry)
+            
+    else:
+        raise ValueError("Invalid label method: " + label_method)
 
-	return score_map, geometry_map
+    assert score_map.shape == (1, 128, 128)
+    assert geometry_map.shape == (8, 128, 128)
+    
+    return score_map, geometry_map
 
 class ImageDataSet(torch.utils.data.Dataset):
 
     def __init__(self, images_dir, annotations_dir):
 
-    	self.images_dir = images_dir
-    	self.annotations_dir = annotations_dir 
-    	self.image_names = list_images(images_dir)
+        self.images_dir = images_dir
+        self.annotations_dir = annotations_dir 
+        self.image_names = list_images(images_dir)
 
     def __getitem__(self, index):
 
-    	image_name = self.image_names[index]
-    	annotation_name = image_name.split(".")[0] + ".csv"
-    	image_path = os.path.join(self.images_dir, image_name)
-    	annotation_path = os.path.join(self.annotations_dir, annotation_name)
+        image_name = self.image_names[index]
+        annotation_name = image_name.split(".")[0] + ".csv"
+        image_path = os.path.join(self.images_dir, image_name)
+        annotation_path = os.path.join(self.annotations_dir, annotation_name)
         # image -> [3, 512, 512]
-    	image = load_image(image_path)
-        print("Image Size:", image.size())
+        image = load_image(image_path)             
         # score_map -> [1, 128, 128]; geometry_map -> [8, 128, 128]
-    	score_map, geometry_map = load_score_and_geometry_map(annotation_path)
+        score_map, geometry_map = load_score_and_geometry_map(annotation_path)
         
         return image, score_map, geometry_map
 
@@ -112,20 +157,22 @@ class ImageDataSet(torch.utils.data.Dataset):
 
 
 # test code
+"""
 train_data_dir = config["train_data_dir"]
 mini_batch_size = config["mini_batch_size"]
 train_images_dir = os.path.join(train_data_dir, "images")
 train_annotations_dir = os.path.join(train_data_dir, "annotations")
 
 trainset = ImageDataSet(train_images_dir, train_annotations_dir)
-trainloader = DataLoader(trainset, batch_size=mini_batch_size, shuffle=True)
+train_loader = torch.utils.data.DataLoader(trainset, batch_size=mini_batch_size, shuffle=True)
 print("Number of examples:", len(trainset))
 print("Mini batch size:", mini_batch_size)
 n_mini_batches = len(trainset)//mini_batch_size + int(len(trainset)%mini_batch_size!=0)
 print("Number of mini batches:", n_mini_batches) 
 
-for i, (image, score_map, geometry_map) in tqdm(enumerate(trainloader), total=n_mini_batches):
-            
+for i, train_eg in tqdm(enumerate(train_loader), total=n_mini_batches):
+    image, score_map, geometry_map = train_eg      
     print(image.size())
     print(score_map.size(), geometry_map.size())
     time.sleep(10)
+"""
