3a6992b356dc2f7bce77886417aac6dd4c7181eb
gokulkarthik <gokulkarthikk@gmail.com>
2019-09-28 15:56:45 +0530

    update files

--- format_data.py
+++ format_data.py
@@ -115,6 +115,7 @@ elif representation == "QUAD_multiple":
                 rough_map = np.zeros([128, 128], dtype="int") # [128, 128]
                 scaled_shape_coords = (shape_coords // 4)# [4, 2]
                 scaled_shape_coords = np.expand_dims(scaled_shape_coords, axis=0).astype("int") # [1, 4, 2]
+                print(scaled_shape_coords)
                 cv2.fillPoly(rough_map, scaled_shape_coords, 1)
                 print(rough_map.sum())
                 points = np.argwhere(rough_map)
