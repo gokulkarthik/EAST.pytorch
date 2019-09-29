eaf4fd08349480cfff3aabba9596f103d120a22c
gokulkarthik <gokulkarthikk@gmail.com>
2019-09-29 23:30:09 +0530

    update files

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class Config:
     test_data_dir = os.path.join(data_dir, 'test')
 
     cuda = True
-    lambda_score = 1
+    lambda_score = 100
     lambda_geometry = 1
     epochs = 50
     smoothed_l1_loss_beta = 1.0
@@ -51,7 +51,12 @@ class Config:
                  "lr_scheduler_step_size": lr_scheduler_step_size,
                  "lr_scheduler_gamma": lr_scheduler_gamma,
                  "mini_batch_size":mini_batch_size,
-                 "comments": "LR:0.01, Model: xavier init; Score Loss: cross entropy with beta; Geo Loss: L1 loss with text mask normalized by 8*512"
+                 "comments": """
+                 LR:0.01, 
+                 Model: xavier init;
+                 Score Loss: 100 * cross entropy with beta; 
+                 Geo Loss: L1 loss with text mask normalized by 8*512
+                 """
                 }
     
     trained_model_file = "./experiment_model/experiment_{}_epoch_{}.pth".format("11", "50") 
